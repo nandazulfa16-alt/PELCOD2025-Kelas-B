907e706baf1e8b2cdadc11648444d5bc5683d9c2
nandazulfa16-alt <nandazulfa16@gmail.com>
2025-09-13 11:42:30 +0700

    250441100135_NandaDay1

---
+++ 250441100135_NandaDay1/Day1.py
@@ -0,0 +1,59 @@
+# print("helloo")
+# print("haiiii")
+# a = 10
+# b = 20
+# print (a + b)
+ 
+# nama = "nanda"
+# umur = 18
+# print (nama, umur)
+
+# a = 10
+# b = 3
+# c = a + b
+# hasil = (a>b) and (b<a)
+# print (c, hasil)
+
+# #f string
+# nama = "nanda"
+# pesan = f"halo, {nama}!"
+# print (pesan)
+# #format string
+# pesan = "halo, {}". format(nama)
+# print(pesan)
+
+# nama = input("masukkan nama : ")
+# print(f"selamat datang, {nama}\n")
+
+# nama = "Nanda Zulfa Septiana"
+# nim = 250441100135
+# kuliah = "UNIVERSITAS TRUNOJOYO MADURA"
+# semester = 1
+# print("===BIODATA MAHASISWA===")
+# print ("Hallo nama saya adalah : " + nama + "\n")
+# print (f"Nim saya : {nim}\n")
+# print (f"Saya berkuliah di : {kuliah}\n")
+# print("Yang baru semester : {}".format(semester))
+# # \n = enter
+
+
+# a = 10
+# b = 5
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+# g = a % b
+# print ("Hasil penjumlahan =", c)
+# print ("Hasil pengurangan =", d)
+# print ("Hasil perkalian =", e)
+# print ("Hasil pembagian =", f)
+# print ("Hasil sisa bagi =", g)
+
+nama = input ("Masukkan nama : ")
+umur = input ("Masukkan umur : ")
+buah = input ("Masukkan buah favorit : ")
+print("\n")
+print ("Nama saya adalah : " + nama)
+print ("Umur saya adalah : " + umur + " tahun")
+print ("Buah favorit saya adalah : " + buah)
